1169a5b82ad8264e81ba89fea8ce1e6c70e4201c
bouthaina <bouthainabakouch@gmail.com>
2025-02-22 15:04:56 +0100

    Add multiplication route

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ router.get('/', (req, res) => {
     res.send(`Addition: ${num1 + num2}`);
 });
 
+router.get('/multip/:num1/:num2', (req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+
+    res.send(`Multiplication: ${num1 * num2}`);
+});
+
 app.use('/',router)
 
 app.listen(3000,()=> console.log ("Server is running"))
