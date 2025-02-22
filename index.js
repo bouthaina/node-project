a30703ace919a5fa7ecb3fe8df0536fcbb2f6d48
bouthaina <bouthainabakouch@gmail.com>
2025-02-22 14:53:33 +0100

    Add addition route

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  router.get('/add/:num1/:num2', (req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+
+    res.send(`Addition: ${num1 + num2}`);
+});
+
+app.use('/',router)
+
+app.listen(3000,()=> console.log ("Server is running"))
